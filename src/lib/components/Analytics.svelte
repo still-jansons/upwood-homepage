93720cf3bfaf6b17812c10569d164670dc955d8c
Arturs <arturs.jansons@va.lv>
2023-11-14 15:31:20 +0200

    Gtag

---
+++ src/lib/components/Analytics.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+
+    $: {
+        if (typeof gtag !== 'undefined') {
+            gtag('config', 'GTM-MNX3ZLNL', {
+                page_title: document.title,
+                page_path: $page.url.pathname,
+            })
+        }
+    }
+</script>
+
+<svelte:head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-MNX3ZLNL"></script>
+    <script>
+        window.dataLayer = window.dataLayer || []
+
+        function gtag() {
+            dataLayer.push(arguments)
+        }
+
+        gtag('js', new Date())
+        gtag('config', 'GTM-MNX3ZLNL')
+    </script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
 	import '../app.pcss';
     import Navbar from '$lib/components/Navbar.svelte';
     import Footer from "$lib/components/Footer.svelte";
+    import Analytics from "$lib/components/Analytics.svelte";
 
     export let data;
 </script>
 
+<Analytics />
 <Navbar />
 <slot />
 <Footer config={data.footerConfig} />
