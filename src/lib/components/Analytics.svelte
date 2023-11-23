89ac41b2f21ba4df2813df413d62e09b953969b4
Arturs <arturs.jansons@va.lv>
2023-11-23 14:36:56 +0200

    Simplify gtag

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -1,28 +1,4 @@
-<script lang="ts">
-    // import { page } from '$app/stores'
-    //
-    // $: {
-    //     if (typeof gtag !== 'undefined') {
-    //         gtag('config', 'GTM-MNX3ZLNL', {
-    //             page_title: document.title,
-    //             page_path: $page.url.pathname,
-    //         })
-    //     }
-    // }
-</script>
-
 <svelte:head>
-<!--    <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-MNX3ZLNL"></script>-->
-<!--    <script>-->
-<!--        window.dataLayer = window.dataLayer || []-->
-
-<!--        function gtag() {-->
-<!--            dataLayer.push(arguments)-->
-<!--        }-->
-
-<!--        gtag('js', new Date())-->
-<!--        gtag('config', 'GTM-MNX3ZLNL')-->
-<!--    </script>-->
     <script>
         (
             function(w,d,s,l,i){
